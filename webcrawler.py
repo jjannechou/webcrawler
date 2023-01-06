a9d748dcf893e5a9fe86ad3ec056fc46efd533e4
jjannechou <122077219+jjannechou@users.noreply.github.com>
2023-01-06 17:48:56 +0800

    Add files via upload

---
+++ webcrawler.py
@@ -0,0 +1,63 @@
+"""
+File: webcrawler.py
+Name: 
+--------------------------
+This file collects more data from
+https://www.ssa.gov/oact/babynames/decades/names2010s.html
+https://www.ssa.gov/oact/babynames/decades/names2000s.html
+https://www.ssa.gov/oact/babynames/decades/names1990s.html
+Please print the number of top200 male and female on Console
+You should see:
+---------------------------
+2010s
+Male number: 10895302
+Female number: 7942376
+---------------------------
+2000s
+Male number: 12976700
+Female number: 9208284
+---------------------------
+1990s
+Male number: 14145953
+Female number: 10644323
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    for year in ['2010s', '2000s', '1990s']:
+        print('---------------------------')
+        print(year)
+        url = 'https://www.ssa.gov/oact/babynames/decades/names'+year+'.html'
+        
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html)
+
+        # ----- Write your code below this line ----- #
+        tags = soup.find_all('table', {'class': 't-stripe'})
+        total_male = 0
+        total_female = 0
+
+        for tag in tags:
+            total = tag.tbody.text.split()
+            count = 0
+            for ele in total:
+                count += 1
+                if count % 5 == 3:    # male
+                    male_number = int(ele.replace(',', ''))
+                    total_male += male_number
+                elif count % 5 == 0:    # female
+                    female_number = int(ele.replace(',', ''))
+                    total_female += female_number
+                if count > 1000:
+                    break
+        print('Male Number:', total_male)
+        print('Female Number:', total_female)
+
+
+
+if __name__ == '__main__':
+    main()
